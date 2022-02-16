3186c8dc7a8da59f317f6d737b6f146818fd6cde
Bruno <bruno.lucas.rodriguez@gmail.com>
2022-02-16 17:48:19 -0300

    using url.parse (deprecated)

--- controllers/url.js
+++ controllers/url.js
@@ -1,16 +1,23 @@
 const dns = require("dns");
 const { URL } = require("url");
+const url = require("url");
 const { createUrl, findUrlById } = require("../services/url-services");
 
 const urlShortener = (req, res, next) => {
-	try {
-		const url_name = new URL(req.body.url);
-		console.log(url_name);
+	//try {
+		// const url_name = new URL(req.body.url);
+		// console.log(url_name);
 
-		dns.lookup(url_name.hostname, (err, address, family) => {
-			if (err) return res.json({ error: err });
+		dns.lookup(url.parse(req.body.url).hostname, (err, address, family) => {
 
-			console.log("address: %j family: IPv%s", address, family);
+            if(!address) return res.json({ error: 'invalid url' });
+
+			//if (err) return res.json({ error: err });
+
+			//console.log("address: %j family: IPv%s", address, family);
+
+            const url_name = url.parse(req.body.url);
+            console.log(url_name);
 
 			createUrl(url_name.href, (err, data) => {
 				if (err) {
@@ -27,10 +34,11 @@ const urlShortener = (req, res, next) => {
 				});
 			});
 		});
-	} catch (error) {
-		console.log(error);
-		return res.json({ error: "invalid url" });
-	}
+	// } 
+    // catch (error) {
+	// 	console.log(error);
+	// 	return res.json({ error: "invalid url" });
+	// }
 };
 
 const findById = (req, res, next) => {
